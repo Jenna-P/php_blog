e1d8df8b1bda6db3c5b4fe7e50008c8b95e79cab
Jenna Petersen <70805517+Jenna-P@users.noreply.github.com>
2022-04-25 16:54:06 +0200

    sign up page

--- signUp.php
+++ signUp.php
@@ -1,47 +1,3 @@
-<?php
-
-$user = $_POST["username"];
-$pass = $_POST["password"];
-$res = "forkert";
-$login_result="Hello! You are logged in";
-$btn = "Logout";
-
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$db = "logindb";
-
-$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
-
-// $conn = OpenCon();
-$msg = "Connected Successfully";
-echo "<script type='text/javascript'>console.log('$msg');</script>";
-
-if($user == "test" && $pass == "1234") {
-    $res = "rigtigt";
- }
- 
- $sql = "SELECT username, password FROM bruger_tabel WHERE username='$user'";
- $result = $conn->query($sql);
- if($result->num_rows == 1) {
-     $row = mysqli_fetch_array($result);
-     if($row['password'] == $pass) {
-         $res = "Success";
-     } else {
-         $login_result = "Please Try again";
-         $res = "User found, password is incorrect!";
-         $btn = "Login";
-     }
- } else {
-    $login_result = "Please Try again";
-     $res = "User not found";
-     $btn = "Login";
- }
-
- $conn -> close();
-
-?>
-
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -53,10 +9,9 @@ if($user == "test" && $pass == "1234") {
 <?php require('lib/header.php'); ?>
     <section class="breadcumb-area bg-img bg-overlay" style="background-image: url(https://storage.needpix.com/rsynced_images/countryside-2371480_1280.jpg);">
         <div class="bradcumbContent">
-            <h2>Login</h2>
+            <h2>Sign Up</h2>
         </div>
     </section>
-    <!-- ##### Login Area Start ##### -->
     <section class="login-area section-padding-100">
         <div class="container">
             <div class="row justify-content-center">
@@ -71,13 +26,17 @@ if($user == "test" && $pass == "1234") {
                                     <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Username">
                                 </div>
                                 <div class="form-group">
+                                    <label for="exampleInputEmail1">Email</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Username">
+                                </div>
+                                <div class="form-group">
                                     <label for="exampleInputPassword1">Password</label>
                                     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                                 </div>
-                                <button type="submit" class="btn oneMusic-btn mt-30">Login</button>
+                                <button type="submit" class="btn oneMusic-btn mt-30">Signup</button>
                             </form>
                             <div>
-                            <p class="text-center">Not a member? <a data-toggle="tab" href="signUp.php">Sign Up</a></p>
+                            <p class="text-center">Do you already have an account? <a data-toggle="tab" href="login.php">Sign In</a></p>
                             </div>
                         </div>
                     </div>
@@ -85,7 +44,6 @@ if($user == "test" && $pass == "1234") {
             </div>
         </div>
     </section>
-    <!-- ##### Login Area End ##### -->
 </body>
 </html>
 
